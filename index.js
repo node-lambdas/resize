6a0e1adeb202b0ee16b123171f38715bb8e715c1
Darlan Alves <me@darlanalv.es>
2020-08-21 17:16:28 +0200

    feat: add function to resize images

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
-import { lambda } from '@node-lambdas/core';
+import { lambda, Format } from '@node-lambdas/core';
+import sharp from 'sharp';
+
+const MAX_DIMENSION = 3840;
+const FORMATS = ['webp', 'png', 'jpeg', 'raw'];
 
 const configutation = {
-  version: 1,
+  version: 2,
+  actions: {
+    default: {
+      input: Format.Buffer,
+      handler: async (input, output) => {
+        const { options } = input;
+        const { width, height } = readDimensions(options);
+        const { format } = options;
+
+        const validFormat = (format && FORMATS.includes(format) && format) || '';
+        const resizeOptions = width && height ? {
+          fit: sharp.fit.inside,
+          withoutEnlargement: !options.zoom
+        } : undefined;
+
+        let image = sharp(input.body).resize(width, height, resizeOptions);
+
+        image = options.withMetadata ? image.withMetadata() : image;
+        image = validFormat ? image[validFormat]() : image;
+
+        output.send(image.toBuffer());
+      },
+    },
+  },
 };
 
-lambda(configutation, (input, output) => input.pipe(output));
+function readDimensions(options) {
+  const inputWidth = Number(options.width) || undefined;
+  const inputHeight = Number(options.height) || undefined;
+
+  const width = inputWidth ? Math.min(MAX_DIMENSION, inputWidth) : inputWidth;
+  const height = inputHeight ? Math.min(MAX_DIMENSION, inputHeight) : inputHeight;
+
+  return {
+    width: width > 0 ? width : undefined,
+    height: height > 0 ? height : undefined,
+  };
+}
+
+lambda(configutation);
